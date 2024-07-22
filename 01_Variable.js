363de3b4d9fb2ba57e013b303fe8228b56af5917
Rahulverma0711 <vermamm122@gmail.com>
2024-07-22 11:33:13 +0530

    const added

---
+++ 01_Variable.js
@@ -0,0 +1,6 @@
+// Variable - A JavaScript varibal eis just a name of storage location.
+// Using let ,const and var keyword we can give that name.
+
+let userName="Rahul Verma";
+console.log(userName);
+const totalHours=24;
